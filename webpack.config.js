64e8d9f20c254c4dd4e5c3aba58e97b5f694e216
Brent Van Minnen <bjvanminnen@gmail.com>
2015-10-16 21:27:16 -0700

    React hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
 var path = require('path');
+var webpack = require('webpack');
 var SuccessPlugin = require('webpack-success-plugin');
 
 var BUILD_DIR = 'build/';
 
 var config = module.exports = {
   entry: [
-    __dirname + '/node_modules/babel-core/browser-polyfill.js',
+    'webpack-hot-middleware/client',
     './src/main.js'
   ],
   output: {
-    filename: BUILD_DIR + '[name].js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   module: {
     loaders: [
       {
         test: new RegExp(__dirname + '\/[src|test].*\.jsx*$'),
         loaders: [
-          'babel-loader?stage=0'
+          'babel'
         ]
       }
    ],
-    noParse : [
-      /\/babel-core\/browser-polyfill\.js$/
-    ],
   },
   resolve: {
     extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx"],
     root: path.resolve(__dirname, "node_modules")
   },
-  plugins: [new SuccessPlugin('node build/main.js')],
-  devtool: "#inline-source-map",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  devtool: "eval",
 }
