5b35ea4f79034e6d69ba4b82f6208e32806b201f
Brent Van Minnen <bjvanminnen@gmail.com>
2015-10-18 11:28:26 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import React from 'react';
 import { App } from './App';
 
 React.render(<App />, document.getElementById('root'));
+
+/**
+User Actions:
+SUBMIT_BID
+
+Controller Actions:
+
+
+Server Actions:
+RECEIVE_STATE
+
+Some sort of error action?
+*/
